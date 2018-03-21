7d9b6ee96dd0b4b916b20e80b78760452409f506
Myoung-Wan Koo <mwkoo9@gmail.com>
2018-03-21 13:56:33 +0900

    Initial Commit

---
+++ FirstApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstApp3
+//
+//  Created by Myoung-Wan Koo on 2018. 3. 21..
+//  Copyright © 2018년 Myoung-Wan Koo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
